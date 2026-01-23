7b7d681c8b5aa5e899fc67865ecd50037fd817f4
Austin <austinwong43@gmail.com>
2026-01-23 09:39:19 +0800

    CORS fix 2

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -15,8 +15,7 @@ const SignupPage = () => {
 
   const currentUser = useContext(AuthContext);
 
-  const url =
-    "https://2024be56-7c75-4898-bba3-bb654ca8b38a-00-iglx0rrqz7e8.sisko.replit.dev";
+  const url = "https://vercel-express-api-capstone.vercel.app";
 
   const handleSignup = async (e) => {
     try {
